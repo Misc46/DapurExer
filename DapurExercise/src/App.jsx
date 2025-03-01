e9433fb91f0a9be28f8982038e920d487ab7b88e
Chordius <malau.jesaya@gmail.com>
2025-03-01 21:56:06 +0700

    added react-router

--- DapurExercise/src/App.jsx
+++ DapurExercise/src/App.jsx
@@ -2,13 +2,23 @@
 import React from "react";
 import Footer from "./Components/Footer";
 import RecipeBrowse from "./Components/RecipeBrowse";
+import CategoryCard from "./Components/CategoryCard";
+import About from "./Components/About";
+import { BrowserRouter, Route, Routes } from "react-router"
 
 const App = () => {
   return (
-    <div>
-      <RecipeBrowse />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<RecipeBrowse />} />
+          <Route path="/browse" element={<RecipeBrowse />} />
+          <Route path="/about" element={<About />} /> 
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
