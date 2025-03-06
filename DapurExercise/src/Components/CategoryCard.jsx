5ba02300b6cacfca92e23b5b946be51a23e680d8
nardaiv <nardaikiru@gmail.com>
2025-03-06 20:53:40 +0700

    update categorycard

--- DapurExercise/src/Components/CategoryCard.jsx
+++ DapurExercise/src/Components/CategoryCard.jsx
@@ -1,9 +1,12 @@
-const CategoryCard = ({ image, name }) => {
+const CategoryCard = ({ image, name, route }) => {
   return (
-    <div className="p-5 border-2 border-[#FFB300] rounded-lg w-48 flex flex-col items-center gap-1.5">
+    <a
+      href={route}
+      className="p-5 border-2 border-[#FFB300] drop-shadow-xl rounded-lg w-44 flex flex-col items-center gap-1.5 justify-around"
+    >
       <img className="w-10/12" src={image} alt="" />
       <p className="text-lg font-bold">{name}</p>
-    </div>
+    </a>
   );
 };
 
--- DapurExercise/src/Components/RecipeBrowse.jsx
+++ DapurExercise/src/Components/RecipeBrowse.jsx
@@ -1,5 +1,8 @@
 import CategoryCard from "./CategoryCard";
 import chicken from "/chicken.png";
+import vegetable from "/vegetable.png";
+import seafood from "/seafood.png";
+import beef from "/beef.png";
 
 const RecipeBrowse = () => {
   return (
@@ -8,10 +11,10 @@ const RecipeBrowse = () => {
         <h2 className="font-bold text-3xl mb-5">Categories:</h2>
 
         <div className="w-full flex justify-center gap-8 flex-wrap">
-          <CategoryCard image={chicken} name={"Chicken"} />
-          <CategoryCard image={chicken} name={"Chicken"} />
-          <CategoryCard image={chicken} name={"Chicken"} />
-          <CategoryCard image={chicken} name={"Chicken"} />
+          <CategoryCard image={chicken} name={"Chicken"} route={"#"} />
+          <CategoryCard image={beef} name={"Beef"} route={"#"} />
+          <CategoryCard image={seafood} name={"Seafood"} route={"#"} />
+          <CategoryCard image={vegetable} name={"Vegetable"} route={"#"} />
         </div>
       </div>
 
