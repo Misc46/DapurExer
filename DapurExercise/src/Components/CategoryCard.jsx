d0210c7e767a86a8c45f1b5d16a2cc09166aeb07
nardaiv <nardaikiru@gmail.com>
2025-03-06 21:51:41 +0700

    add recipe card

--- DapurExercise/src/Components/CategoryCard.jsx
+++ DapurExercise/src/Components/CategoryCard.jsx
@@ -2,7 +2,7 @@ const CategoryCard = ({ image, name, route }) => {
   return (
     <a
       href={route}
-      className="p-5 border-2 border-[#FFB300] drop-shadow-xl rounded-lg w-44 flex flex-col items-center gap-1.5 justify-around"
+      className="flex w-44 flex-col items-center justify-around gap-1.5 rounded-lg border-2 border-[#FFB300] p-5 shadow-xl"
     >
       <img className="w-10/12" src={image} alt="" />
       <p className="text-lg font-bold">{name}</p>
--- DapurExercise/src/Components/RecipeBrowse.jsx
+++ DapurExercise/src/Components/RecipeBrowse.jsx
@@ -1,4 +1,5 @@
 import CategoryCard from "./CategoryCard";
+import RecipeCard from "./RecipeCard";
 import chicken from "/chicken.png";
 import vegetable from "/vegetable.png";
 import seafood from "/seafood.png";
@@ -6,11 +7,11 @@ import beef from "/beef.png";
 
 const RecipeBrowse = () => {
   return (
-    <div className="p-10 mx-10">
+    <div className="mx-10 p-10">
       <div>
-        <h2 className="font-bold text-3xl mb-5">Categories:</h2>
+        <h2 className="mb-5 text-3xl font-bold">Categories:</h2>
 
-        <div className="w-full flex justify-center gap-8 flex-wrap">
+        <div className="flex w-full flex-wrap justify-center gap-8">
           <CategoryCard image={chicken} name={"Chicken"} route={"#"} />
           <CategoryCard image={beef} name={"Beef"} route={"#"} />
           <CategoryCard image={seafood} name={"Seafood"} route={"#"} />
@@ -19,9 +20,20 @@ const RecipeBrowse = () => {
       </div>
 
       <div>
-        <h2 className="font-bold text-3xl mb-5">Recipes:</h2>
+        <h2 className="mb-5 text-3xl font-bold">Recipes:</h2>
 
-        <div></div>
+        <div className="mx-auto flex flex-wrap justify-center gap-8">
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+        </div>
       </div>
     </div>
   );
